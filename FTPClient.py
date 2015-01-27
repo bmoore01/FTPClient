53a61d3880f9722b6d91b9c0170cbaae625000d5
cmdz0rd <benmoore422@gmail.com>
2015-01-27 16:27:43 +0000

    Updated fileTransfer class
    System can now establish a  connection with a remote server.

--- FTPClient.py
+++ FTPClient.py
@@ -1,7 +1,8 @@
 import socket
 import sys
 #Used for exit command
-
+#Planning to implement classes into a module
+#import fileTransfer
 
 
 def main():
@@ -11,9 +12,10 @@ def main():
             sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         except socket.error, msg:
             print "Failed to create socket, error code : " + str(msg[0]) + " Error message : " + str(msg[1])
-        def __init__(self,host,port):
+        def __init__(self,host,port,remote_ip):
             self.host = host
             self.port = port
+            self.remote_ip = remote_ip
         def getConnectionInfo(self):
             print"Please enter the address for the server you're trying to connect to"
             self.host = raw_input(">>")
@@ -21,32 +23,31 @@ def main():
             self.port = raw_input(">>")
             if len(str(self.port)) == 0:
                 self.port = 21
+            try:
+                self.remote_ip = socket.gethostbyname( self.host )
+                print self.remote_ip
+            except socket.gaierror:
+                #Line below for bug fixing
+                print "hostname could not resolve"
+                sys.exit()
+        def connectSocket(self):
+            self.sock.connect((self.remote_ip, int(self.port)))
+            print "Socket connection successful"
 
     print "Welcome to FTPy!"
     print 'Would you like to start a file tansfer?y/n :'
     start = raw_input(">>")
     if start[0].lower() == 'y':
-        session = fileTransfer(0,0)
+        session = fileTransfer(0,0,0)
         session.getConnectionInfo()
+        session.connectSocket()
     elif start[0].lower() == 'n':
-        print "Closing program"
+        print "Closing program."
         sys.exit()
 
 
-
-
-
-    try:
-        remote_ip = socket.gethostbyname( session.host )
-    except socket.gaierror:
-        #Could not resolve
-        print "hostname could not resolve, are you connected to the internet?"
-        sys.exit()
-
-
-    print "You have connected to " + str(session.host) + " the IP is " + str(remote_ip) + " You are connected on port " + str(session.port)
-
-
+    print "You have connected to " + str(session.host) + " the IP is " + str(session.remote_ip) + " You are connected on port " + str(session.port)
+    print "Socket connection complete!"
 
 if __name__ == '__main__':
     main()
